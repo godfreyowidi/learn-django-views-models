95c88ca8289b8960ead38d557b8319bbe6b0e89a
Godfrey Owidi <godfreyowiidi@gmail.com>
2022-12-14 23:30:18 +0300

    Add search, login

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,37 @@ from django.shortcuts import render
 
 from .models import Articles
 
+
+def article_search_view(request):
+    # print(dir(request))
+    # print(request.GET)
+    query_dict = request.GET
+    # query = query_dict.get('query')
+    try:
+        query = int(query_dict.get('query'))
+    except:
+        query = None
+
+    article_obj = None
+    if query is not None:
+        article_obj = Articles.objects.get(id=query)
+    context={
+        "object": article_obj,
+    }
+    return render(request, "articles/search.html", context=context)
+
+def article_create_view(request):
+    # print(request.POST)
+    context = {}
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        articles_obj = Articles.objects.create(title=title, content=content)
+        print(title, content)
+        context["object"] = articles_obj
+        context["created"] = True
+    return render(request, 'articles/create.html', context=context)
+
 def articles_details_view(request, id=None):
     article_obj = None
     if id is not None:
--- learndjango/urls.py
+++ learndjango/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
-from articles import views
+from articles.views import (
+    article_search_view,
+    article_create_view,
+    articles_details_view,
+)
+from accounts.views import login_view
 
 urlpatterns = [
     path('', home_view),
-    path('articles/<int:id>', views.articles_details_view),
+    path('articles/', article_search_view),
+    path('articles/create/', article_create_view),
+    path('articles/<int:id>', articles_details_view),
     path('admin/', admin.site.urls),
+    path('login/', login_view),
 ]
